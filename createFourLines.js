f33e0714e35577f98d83470c8100fd2b6f03aa89
Mohsin Raees <160493218+MohsinRaees@users.noreply.github.com>
2024-02-21 21:58:27 +0500

    Add files via upload

---
+++ createFourLines.js
@@ -0,0 +1,11 @@
+//You should create four lines that look like this://
+//console.log(5 + 3)//
+console.log("-------------------------------------");
+console.log(5 + 3);
+console.log("-------------------------------------");
+console.log(11 - 3);
+console.log("-------------------------------------");
+console.log(2 * 4);
+console.log("-------------------------------------");
+console.log(16 / 2);
+console.log("-------------------------------------");
